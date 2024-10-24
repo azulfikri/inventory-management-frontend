f70e8937ffd1c2e6752f0865a9d077ba2ebe18f3
azulfikri <achamadzulfikri20@gmail.com>
2024-10-24 10:45:54 +0700

    add pinia and axios

--- src/main.js
+++ src/main.js
@@ -3,5 +3,7 @@ import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import router from './router'
+import { createPinia } from 'pinia'
+import axios from 'axios';
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(createPinia()).use(router).mount('#app')
