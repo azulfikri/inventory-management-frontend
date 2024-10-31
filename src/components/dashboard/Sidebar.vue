d1d784cbd2e8dc1491b5ab19a2f2f0fc336648a9
azulfikri <achamadzulfikri20@gmail.com>
2024-11-01 00:57:14 +0700

    add scss file and update dashboard components with bootstrap class

--- src/components/dashboard/Sidebar.vue
+++ src/components/dashboard/Sidebar.vue
@@ -1,28 +1,48 @@
 <template>
   <div :class="['sidebar', { 'd-none': !isSidebarVisible }]">
-    <a class="logo">SIMBAR</a>
-    <ul>
-      <li v-if="currentRole === 'admin'">
-        <a href="#" @click.prevent="showComponent('users')">
-          <i class="fas fa-home"></i> Users
+    <a
+      href="#"
+      class="logo d-flex justify-content-center align-items-center text-center mb-4 text-white text-decoration-none fw-bold fs-5"
+      >SIMBAR</a
+    >
+    <ul class="nav flex-column">
+      <li v-if="currentRole === 'admin'" class="nav-item mb-2">
+        <a
+          href="#"
+          @click.prevent="showComponent('users')"
+          class="nav-link text-white d-flex align-items-center"
+        >
+          <i class="bi bi-house-door me-2"></i> Users
         </a>
       </li>
 
-      <li>
-        <a href="#" @click.prevent="showComponent('items')">
-          <i class="fas fa-box"></i> Items
+      <li class="nav-item mb-2">
+        <a
+          href="#"
+          @click.prevent="showComponent('items')"
+          class="nav-link text-white d-flex align-items-center"
+        >
+          <i class="bi bi-box me-2"></i> Items
         </a>
       </li>
 
-      <li>
-        <a href="#" @click.prevent="showComponent('transactions')">
-          <i class="fas fa-exchange-alt"></i> Transactions
+      <li class="nav-item">
+        <a
+          href="#"
+          @click.prevent="showComponent('transactions')"
+          class="nav-link text-white d-flex align-items-center"
+        >
+          <i class="bi bi-cash me-2"></i> Transactions
         </a>
       </li>
 
-      <li v-if="currentRole == 'user'">
-        <a href="#" @click.prevent="showComponent('history')">
-          <i class="fas fa-history"></i> History
+      <li v-if="currentRole == 'user'" class="nav-item">
+        <a
+          href="#"
+          @click.prevent="showComponent('history')"
+          class="nav-link text-white d-flex align-items-center"
+        >
+          <i class="bi bi-clock-history me-2"></i> History
         </a>
       </li>
     </ul>
@@ -56,52 +76,52 @@ export default {
   color: white;
   padding: 14px;
   height: 100vh;
-  display: flex;
-  flex-direction: column;
+  cursor: pointer;
   position: fixed;
   top: 0;
   left: 0;
   z-index: 999;
   font-size: 18px;
   font-family: sans-serif;
-  transition: transform 0.3s ease;
+  transition: transform 0.3s ease-in-out;
+  transform: translateX(-2);
 }
-.d-none {
-  display: none;
+.sidebar .d-none {
+  transform: translateX(-200px);
 }
-.sidebar.hidden {
+/* .sidebar.hidden {
   transform: translateX(-100%);
-}
-.logo {
+} */
+/* .logo {
+  color: white;
   font-size: 24px;
   margin-bottom: 30px;
   font-weight: bold;
   text-align: center;
-}
-ul {
+} */
+/* ul {
   list-style: none;
   padding: 0;
   margin: 0;
-}
-li {
+} */
+/* li {
   margin-bottom: 20px;
   text-align: center;
-}
-a {
+} */
+.nav-link {
   color: white;
-  text-decoration: none;
   cursor: pointer;
   display: flex;
-  align-items: center;
   justify-content: center;
+  align-items: center;
   padding: 10px 0;
   transition: background-color 0.3s ease;
 }
-a:hover {
+.nav-link:hover {
   text-decoration: underline;
   background-color: rgba(255, 255, 255, 0.1);
 }
-a i {
-  margin-right: 8px;
+.me-2 {
+  margin-right: 0.5rem;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import './assets/styles/global.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.min.css';
 // import router from './router'
 import { createPinia } from 'pinia'
 // import axios from 'axios';
