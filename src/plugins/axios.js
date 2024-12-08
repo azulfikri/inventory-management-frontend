f5ae1f61b9bf33443463837c04da038e3f23551a
azulfikri <achamadzulfikri20@gmail.com>
2024-12-08 15:57:34 +0700

    test deploy 3

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useAuthStore } from "@/store/authStore";
 const apiClient = axios.create({
-  baseURL: "https://inventory-management-backend-six-xi.vercel.app/api",
+  baseURL: process.env.VUE_APP_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
